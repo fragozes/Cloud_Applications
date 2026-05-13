960d49f1643ad5682c13125646c4e24f3b05c34f
Luiz Fragozes <28720450+fragozes@users.noreply.github.com>
2026-05-13 17:07:03 +0100

    test_user_and_deployGitHub

--- seeds/users.sql
+++ seeds/users.sql
@@ -5,7 +5,7 @@ CREATE SEQUENCE IF NOT EXISTS users_id_seq;
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     username VARCHAR(20) UNIQUE NOT NULL,
-    password VARCHAR(8) NOT NULL
+    password VARCHAR(20) NOT NULL
 );
 
 INSERT INTO users (username, password) VALUES ('Luiz_frgzs', 'lu1z123!');
